727de2edc752a5f4e56d8ed6ccf77bdd4fb0bfd9
Maksim Dauhaleu <maksimdauhaleu@macbook.local>
2023-05-12 11:58:24 -0400

    Update

--- features/steps/amazon_cart_creative.py
+++ features/steps/amazon_cart_creative.py
@@ -2,12 +2,14 @@ from selenium.webdriver.common.by import By
 from behave import given, when, then
 from time import sleep
 
+
 @then("Open an item and add to cart")
 def add_to_cart(context):
     context.driver.find_element(By.XPATH, "//a[contains(@href, 'Writing-Computer-Industrial-Folding-Notebook')]").click()
     context.driver.find_element(By.ID, "add-to-cart-button").click()
     context.driver.find_element(By.ID, "attachSiNoCoverage").click()
     sleep(5)
+
 
 @then("Check if the item is in a cart")
 def cart_count_check(context):
