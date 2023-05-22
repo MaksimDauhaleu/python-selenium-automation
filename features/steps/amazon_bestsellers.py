5bad28daded43e00f46385ac17dfab440e958ac9
Maksim Dauhaleu <maksimdauhaleu@macbook.mynetworksettings.com>
2023-05-22 16:05:59 -0400

    Lesson 4 HW

---
+++ features/steps/amazon_bestsellers.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from time import sleep
+
+TAB_LINKS = (By.XPATH, "//div[contains(@class, '_p13n-zg-nav-tab-all_style_zg-tabs-li')]")
+
+
+@given('Open Bestsellers Amazon page')
+def open_bestsellers_amazon(context):
+    context.driver.get('https://www.amazon.com/gp/bestsellers/?ref_=nav_cs_bestsellers')
+
+
+@then('Verify there are {expected_amount} Bestseller links')
+def verify_links(context, expected_amount):
+    expected_amount = int(expected_amount)
+    links_count = len(context.driver.find_elements(*TAB_LINKS))
+    assert links_count == expected_amount, f'Expected {expected_amount} links, but got {links_count}'
+
+
+
---
+++ features/tests/amazon_bestsellers.feature
@@ -0,0 +1,7 @@
+# Created by maksimdauhaleu at 5/20/23
+Feature: # Enter feature name here
+  # Enter feature description here
+
+  Scenario: BestSellers page test case
+    Given Open Bestsellers Amazon page
+    Then Verify there are 5 Bestseller links
